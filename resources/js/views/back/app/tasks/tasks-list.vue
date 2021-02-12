2a536f69e456e24c1965b8cefcd8ae060af24a05
SymmetryDesign <31937849+SymmetryDesign@users.noreply.github.com>
2021-02-12 09:18:50 +0100

    Update tasks-list.vue

--- resources/js/views/back/app/tasks/tasks-list.vue
+++ resources/js/views/back/app/tasks/tasks-list.vue
@@ -21,6 +21,12 @@
                                 </a>
                              
                             </div>
+                            
+                            <a target="_blank" :href="task.jamboard_url" class="btn btn-indigo">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current">
+                                                        <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z"/>
+                                                    </svg>
+                                                </a>
 
                             <div class="mt-1 sm:mt-0 md:col-span-2">
                                 <div>
@@ -35,15 +41,7 @@
                                     </div>
                                     
                                     
-                                    <div class="flex items-center text-sm leading-5">
-                                        <a :href="route('app:projects.show', {project: task.project.uuid})" class="flex items-center text-sm rounded-lg mb-1 hover:underline">
-                                            <div class="w-3 h-3 overflow-hidden rounded-full mr-2.5 hidden sm:inline-block">
-                                                <div class="w-full h-full" :class="['bg-' + task.project.color + '-400']"></div>
-                                            </div>
-
-                                            {{ task.project.name }}
-                                        </a>
-                                    </div>
+                              
                                     
 
                                     <div class="flex items-center text-sm leading-5 mt-0 sm:mt-2">
