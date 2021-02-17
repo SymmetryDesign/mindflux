48141d17188bb515e427c823b61add98a4200f29
wakkarkhan <muhammadwakkar777@gmail.com>
2021-02-18 00:14:17 +0500

    18-Feb after pushing Clint code

--- resources/js/views/back/app/projects/show.vue
+++ resources/js/views/back/app/projects/show.vue
@@ -2,7 +2,7 @@
     <v-app-default-layout>
         <template v-slot:header>
             <h1 class="text-2xl font-semibold text-gray-900">{{ project.name }}</h1>
-            <div class="flex items-center px-3" v-if="project.jamboard_url != null">
+            <div class="large-jam flex items-center px-3" v-if="project.jamboard_url != null">
                     <a target="_blank" :href="project.jamboard_url"  class="btn btn-indigo">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current">
                             <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z"/>
