3b33e62827699e9c24979e54cea3eb8bbe736ef2
wakkarkhan <muhammadwakkar777@gmail.com>
2021-02-19 16:09:40 +0500

    19-feb after changing class from svg to anchor

--- resources/js/views/back/app/projects/card-task.vue
+++ resources/js/views/back/app/projects/card-task.vue
@@ -55,8 +55,8 @@
             </div>
               <!-- Waqar Code -->
                 <div class="jam flex items-center px-3" v-if="task.jamboard_url">
-                    <a target="_blank" :href="task.jamboard_url" class="btn btn-indigo">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current jam-btn">
+                    <a target="_blank" :href="task.jamboard_url" class="btn btn-indigo jam-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current ">
                             <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z"/>
                         </svg>
                     </a>
